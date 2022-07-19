5f18bd877c0e33f6da9a6f6878acf785aac6623a
Aaron <aaronstz@hotmail.com>
2022-07-19 17:51:53 -0300

    Probando las reglas del PR

--- PF-Rebirth/api/src/models/Owner.js
+++ PF-Rebirth/api/src/models/Owner.js
@@ -5,16 +5,6 @@ module.exports = (sequelize) => {
         name: {
             type: DataTypes.STRING,
             allowNull: false
-        },
-        id: {
-            type: DataTypes.UUID,
-            allowNull: false,
-            primaryKey: true,
-            defaultValue: DataTypes.UUIDV4
-        },
-        image: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
+        }
     })
 }
