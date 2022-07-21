3ea7d8082c30c90ca02377403921a79388567610
Enzosanchezz <enzosanchez771@gmail.com>
2022-07-21 11:32:18 -0300

    feat(api/src/models/SucessStories, agregue el modelo sucessStories

---
+++ PF-Rebirth/api/src/models/SuccessStories.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("successStories", {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey : true,
+    },
+    currentOwnerUser : {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey : true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    image:{
+        type: DataTypes.TEXT,
+    },
+    rating:{
+        type : DataTypes.INTEGER,
+        validate : {
+            min : 0,
+            max : 5,
+        }
+    },
+    description :{
+        type : DataTypes.STRING,
+    },
+    show : {
+        type : DataTypes.BOOLEAN,
+        defaultValue: true,
+    },
+
+  });
+};
