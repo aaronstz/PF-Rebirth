74abb7851e9da5fdc3e535fdbaa9435dfc85dd02
lucasdiaz7 <lucasdiaz687@gmail.com>
2022-07-21 11:11:17 -0300

    feat(src/models/User.js) actualizo usuario

--- PF-Rebirth/api/src/models/User.js
+++ PF-Rebirth/api/src/models/User.js
@@ -2,15 +2,59 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
     sequelize.define('user', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
         id: {
             type: DataTypes.UUID,
             allowNull: false,
             primaryKey: true,
             defaultValue: DataTypes.UUIDV4
+        },
+        userName: {
+            type: DataTypes.STRING
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName:{
+            type: DataTypes.STRING
+        },
+        gender:{
+            type: DataTypes.ENUM("man", "woman"),
+            allowNull: false
+        },
+        address: {
+            type: DataTypes.STRING
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            validate: {
+                isNumeric: true
+            }
+        },
+        mail: {
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+        phone: {
+            type: DataTypes.INTEGER
+        },
+        active:{
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            validate: {
+               len: { 
+                  args: [7, 42],
+                  msg: "The password length should be between 7 and 42 characters."
+               }
+            }
+         },
+        image: {
+            type: DataTypes.STRING
         }
     })
 }
