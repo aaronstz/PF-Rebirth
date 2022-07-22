b8af2c6f3182a0f919451def7809f06b08b4b9b0
Carlos Candioti <carlos.candioti@gmail.com>
2022-07-22 16:24:59 -0300

    Images transfer to src

--- PF-Rebirth/client/src/Components/Header/Header.jsx
+++ PF-Rebirth/client/src/Components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import CarouselFade from '../Carousel/Carousel'
 import {NavLink} from 'react-router-dom'
+import headerImageTeam from "../../Assets/Header-images/botCat.png"
+import headerImageDonation from "../../Assets/Header-images/btnDonate.png"
 import './Header.css'
 function Header() {
   return (
@@ -11,10 +13,10 @@ function Header() {
             </div>
             <div className='header-buttons'>
                 <div className='header-team'>
-                <NavLink to={'/Home'}><img src={"./Header-images/botCat.png"} alt="botonTeam" /></NavLink> 
+                <NavLink to={'/Home'}><img src={headerImageTeam} alt="botonTeam" /></NavLink> 
                 </div>
                 <div className='header-donations'>
-                <NavLink to={'/Home'}><img src={"./Header-images/btnDonate.png"} alt="botonTeam" /></NavLink>
+                <NavLink to={'/Home'}><img src={headerImageDonation} alt="botonTeam" /></NavLink>
                 </div>
             </div>
         </div>
