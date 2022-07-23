5e1e8cbc8f84be0e99e2d78bc40ed0f488d0a098
Carlos Candioti <carlos.candioti@gmail.com>
2022-07-23 14:29:21 -0300

    fix header

--- PF-Rebirth/client/src/App.js
+++ PF-Rebirth/client/src/App.js
@@ -6,7 +6,7 @@ import FAQs from "./Pages/FAQs";
 import Login from "./Pages/Login";
 // import Details from "./Pages/Details";
 // import About from "./Pages/About";
-// import Support from "./Pages/Support";
+import Support from "./Pages/Support";
 // import Terms from "./Pages/Terms";
 // import Privacy from "./Pages/Privacy";
 // import NotFound from "./Components/404/404";
@@ -20,11 +20,12 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/faqs" element={<FAQs />} />
         <Route path="/login" element={<Login />} />
+        <Route path = '/support' element = {<Support/>}/>
         {/*
           <Route path = '/home/:id' element = {<Details/>}/>
           <Route exact path="/" element={<LandingPage />} />
           <Route path = '/about' element = {<About/>}/>
-          <Route path = '/support' element = {<Support/>}/>
+          
           <Route path = '/terms' element = {<Terms/>}/>
           <Route path = '/privacy' element = {<Privacy/>}/>
           <Route path = '*' element = {<NotFound/>}/> */}
