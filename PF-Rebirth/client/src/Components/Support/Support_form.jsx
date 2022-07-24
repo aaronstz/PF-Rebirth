a81b492b0f48aa8af5c82de0e7a8520afb0912e3
Carlos Candioti <carlos.candioti@gmail.com>
2022-07-24 00:46:32 -0300

    Form-controlled from support page ready

--- PF-Rebirth/client/src/Components/Support/Support_form.jsx
+++ PF-Rebirth/client/src/Components/Support/Support_form.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { useState } from "react";
+import '../Support/Support.css'
 
 
 function Support_form(){
-let [supportForm,setSupportForm]=useState({email:"",title:"",description:""})
-let [sFormError,setSFormError]=useState({emailError:"",titleError:"",descriptionError:""})
+let [supportForm,setSupportForm]=useState({email:"",subject:"",description:""})
+let [sFormError,setSFormError]=useState({emailError:"",subjectError:"",descriptionError:""})
 let regexEmail=/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
 
 function handleSubmit(event){
     event.preventDefault()
-   if (supportForm.email==="") setSFormError({...sFormError,emailError:""})
-    
-}
+    let FormErrorData=sFormError;
+    if (regexEmail.test(supportForm.email)) FormErrorData={...FormErrorData,emailError:""}
+    if (supportForm.email==="") FormErrorData={...FormErrorData,emailError:"Email is required"};
+   if (supportForm.subject==="") FormErrorData={...FormErrorData,subjectError:"Subject is required"};
+   else FormErrorData={...FormErrorData,subjectError:""}
+   if (supportForm.description==="") FormErrorData={...FormErrorData,descriptionError:"Description is required"};
+   else FormErrorData={...FormErrorData,descriptionError:""}
+  setSFormError({...FormErrorData})
+  if (FormErrorData.emailError==="" && FormErrorData.subjectError==="" && FormErrorData.descriptionError===""){
+    alert("se pueden enviar los datos")
+  }
+   }
 
 function handleChange(event){
     let field=event.target.name;
@@ -19,16 +29,18 @@ function handleChange(event){
     switch (field){
 case "email":
     if (!regexEmail.test(value)){
-     setSFormError({...sFormError,emailError:"El mail no es valido"})
+     setSFormError({...sFormError,emailError:"Email invalid"})
     }else{
         setSFormError({...sFormError,emailError:""})
     }
     setSupportForm({...supportForm,email:value })
 break
-case "title":
-    setSupportForm({...supportForm,title:value })
+case "subject":
+   if (!(value==="")) setSFormError({...sFormError,subjectError:""})
+    setSupportForm({...supportForm,subject:value })
 break
 case "description":
+    if (!(value==="")) setSFormError({...sFormError,descriptionError:""})
     setSupportForm({...supportForm,description:value })
 break
 default:
@@ -38,16 +50,20 @@ default:
 
 return(
     <React.Fragment>
-<form onSubmit={e=>handleSubmit(e)}>
-<label htmlFor="email">Email</label>
-<input type={"text"} name={"email"} onChange={e=>handleChange(e)} value={supportForm.email} placeholder={"Ingrese su mail"}></input>
-<label >{sFormError.emailError}</label>
-<label htmlFor="title">Email</label>
-<input type={"text"} name={"title"} onChange={e=>handleChange(e)} value={supportForm.title} placeholder={"Ingrese su mail"}></input>
-<label htmlFor="description">Email</label>
-<textarea name={"description"} onChange={e=>handleChange(e)} value={supportForm.description} placeholder={"Ingrese su mail"}></textarea>
-<input type={"submit"} value={"Submit"}></input>
+<div className="support-container">
+    <form onSubmit={e=>handleSubmit(e)}>
+        <div><label htmlFor="email">Email:</label></div>
+        <div><input type={"text"} name={"email"} onChange={e=>handleChange(e)} value={supportForm.email} placeholder={"Email"}></input></div>
+        <div><label className="error">{sFormError.emailError}</label></div>
+        <div><label htmlFor="subject">Subject:</label></div>
+        <div><input type={"text"} name={"subject"} onChange={e=>handleChange(e)} value={supportForm.subject} placeholder={"Subject"}></input></div>
+        <div><label className="error">{sFormError.subjectError}</label></div>
+        <div><label htmlFor="description">Description:</label></div>
+        <div><textarea name={"description"} onChange={e=>handleChange(e)} value={supportForm.description} placeholder={"Describe the problem"}></textarea></div>
+        <div><label className="error">{sFormError.descriptionError}</label></div>
+        <div><input id="submit" type={"submit"} value={"Submit"}></input></div>
 </form>
+</div>
     </React.Fragment>
 )
 
