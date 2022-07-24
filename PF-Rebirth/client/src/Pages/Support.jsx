a9cf00ab784da8d71cdc33cf6fa2fce71094a743
LennySnaider <lenny_snaiderman@yahoo.com>
2022-07-24 13:09:43 -0500

    fixes pequeños de nav y footer en paginas

--- PF-Rebirth/client/src/Pages/Support.jsx
+++ PF-Rebirth/client/src/Pages/Support.jsx
@@ -3,25 +3,25 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Navbar from "../Components/Navbar/Navbar";
-import Footer from '../Components/Footer/Footer';
-import Header from '../Components/Header/Header';
-import SupportForm from '../Components/Support/Support_form'
+import Footer from "../Components/Footer/Footer";
+import Header from "../Components/Header/Header";
+import SupportForm from "../Components/Support/Support_form";
 
 function Support() {
   return (
-    <Container>
-      <Navbar/>
-      <Header/>
-      <Row>
-        <Col>
-
-        <SupportForm/>
-        
-        </Col>
-      </Row>
-      <br />
-     <Footer/>
-    </Container>
+    <>
+      <Navbar />
+      <Container>
+        <Header />
+        <Row>
+          <Col>
+            <SupportForm />
+          </Col>
+        </Row>
+        <br />
+      </Container>
+      <Footer />
+    </>
   );
 }
 
