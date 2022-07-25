ff6691bb60a4e60cf7d87452b6eb5ceed73ea0e6
WillDz90 <will.diazor@gmail.com>
2022-07-25 14:29:13 -0500

    Mejora en el componente testimonials

--- PF-Rebirth/client/src/index.js
+++ PF-Rebirth/client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './Redux/Store/index';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <Provider store = { store }>
