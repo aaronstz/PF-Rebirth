0e516f23a6aebd7e2228e9d3c8a8a78b8287458c
LennySnaider <lenny_snaiderman@yahoo.com>
2022-07-24 21:00:45 -0500

    pagina about us

--- PF-Rebirth/client/src/Pages/Home.jsx
+++ PF-Rebirth/client/src/Pages/Home.jsx
@@ -8,10 +8,10 @@ import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
 import Testimonials from "../Components/Testimonials/Testimonials.jsx";
 import "../index.css";
-import { getPets, getPetNames, getPetFilters } from "../Redux/Actions/index.js";
+import { getPetNames, getPetFilters } from "../Redux/Actions/index.js";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function Home() {
   const petType = useLocation().search?.replace("?type=", "");
