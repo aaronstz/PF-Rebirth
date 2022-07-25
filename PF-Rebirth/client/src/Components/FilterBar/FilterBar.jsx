e8d834a89184434e8a1814d77b3b6e6487d8ed77
Mariel Gramajo <marielgramajo@MacBook-Air-de-Mariel.local>
2022-07-24 21:53:54 -0300

    feat(client/components/filterbar) modifique filter, actions y reducer

--- PF-Rebirth/client/src/Components/FilterBar/FilterBar.jsx
+++ PF-Rebirth/client/src/Components/FilterBar/FilterBar.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import "./FilterBar.css";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import {
+  filterBySex,
+  filterBySize,
+  getPets,
+  orderByAge,
+} from "../../Redux/Actions";
+import { useDispatch } from "react-redux";
 
 function FiltersBar() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(orderByAge());
+    dispatch(filterBySex());
+    dispatch(filterBySize());
+  }, [dispatch]);
+
+  function handleOrderByAge(e) {
+    dispatch(orderByAge(e));
+  }
+
+  function handleFilterBySex(e) {
+    dispatch(filterBySex(e));
+  }
+  function handleFilterBySize(e) {
+    dispatch(filterBySize(e));
+  }
+
   return (
     <div className="filtBar">
       <DropdownButton
@@ -23,21 +49,17 @@ function FiltersBar() {
       </DropdownButton>
 
       <DropdownButton
+        onSelect={(e) => {
+          handleOrderByAge(e);
+        }}
         id="dropdown-button-light"
         variant="light"
         menuVariant="light"
         title="AGE"
         className="ms-2"
       >
-        <Dropdown.Item value="age" href="#/action-1" active>
-          all
-        </Dropdown.Item>
-        <Dropdown.Item value="young" href="#/action-2">
-          young
-        </Dropdown.Item>
-        <Dropdown.Item value="old" href="#/action-3">
-          old
-        </Dropdown.Item>
+        <Dropdown.Item eventKey="young">young</Dropdown.Item>
+        <Dropdown.Item eventKey="old">old</Dropdown.Item>
       </DropdownButton>
 
       <InputGroup className="ms-2 w-50">
@@ -50,30 +72,33 @@ function FiltersBar() {
       </InputGroup>
 
       <DropdownButton
+        onSelect={(e) => {
+          handleFilterBySex(e);
+        }}
         id="dropdown-button-light"
         variant="light"
         menuVariant="light"
         title="SEX"
         className="ms-2"
       >
-        <Dropdown.Item href="#/action-1" active>
-          Male
-        </Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Female</Dropdown.Item>
+        <Dropdown.Item eventKey="All">All</Dropdown.Item>
+        <Dropdown.Item eventKey="Male">Male</Dropdown.Item>
+        <Dropdown.Item eventKey="Female">Female</Dropdown.Item>
       </DropdownButton>
       <DropdownButton
+        onSelect={(e) => {
+          handleFilterBySize(e);
+        }}
         id="dropdown-button-light"
         variant="light"
         menuVariant="light"
         title="SIZE"
         className="ms-2"
       >
-        <Dropdown.Item href="#/action-1" active>
-          Tiny
-        </Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Small</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Medium</Dropdown.Item>
-        <Dropdown.Item href="#/action-4">Big</Dropdown.Item>
+        <Dropdown.Item eventKey="Any">Any</Dropdown.Item>
+        <Dropdown.Item eventKey="small">Small</Dropdown.Item>
+        <Dropdown.Item eventKey="medium">Medium</Dropdown.Item>
+        <Dropdown.Item eventKey="big">Big</Dropdown.Item>
       </DropdownButton>
     </div>
   );
--- PF-Rebirth/client/src/Redux/Actions/index.js
+++ PF-Rebirth/client/src/Redux/Actions/index.js
@@ -1,153 +1,152 @@
-import axios from 'axios';
+import axios from "axios";
 
-export function getUsers(){
-    return async function(dispatch){
-        try {
-            const json = await axios('http://localhost:3001/user')
-            return dispatch({
-                type: 'GET_USER',
-                payload: json.data
-            })
-        } catch (error) {
-            console.log(error)
-            alert('No user found')
-        }
+export function getUsers() {
+  return async function (dispatch) {
+    try {
+      const json = await axios("http://localhost:3001/user");
+      return dispatch({
+        type: "GET_USER",
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log(error);
+      alert("No user found");
     }
+  };
 }
 
-export function getUserId(id){
-    return async function(dispatch){
-        try {
-            const json = await axios(`http://localhost:3001/user/${id}`)
-            return dispatch({
-                type: 'GET_USER_ID',
-                payload: json.data
-            })
-        } catch (error) {
-            console.log(error)
-            alert('No user found')
-        }
+export function getUserId(id) {
+  return async function (dispatch) {
+    try {
+      const json = await axios(`http://localhost:3001/user/${id}`);
+      return dispatch({
+        type: "GET_USER_ID",
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log(error);
+      alert("No user found");
     }
-
+  };
 }
 
-export function postUser(payload){
-    return async function(dispatch){
-        await axios.post('http://localhost:3001/user' + payload)
-    }
+export function postUser(payload) {
+  return async function (dispatch) {
+    await axios.post("http://localhost:3001/user" + payload);
+  };
 }
 
-export function deleteUser(id){
-    return async function dispatch(){
-        try {
-            const json = await axios.delete(`http://localhost:3001/user/${id}`);
-            return dispatch({
-                type: 'DELETE_USER',
-                payload: json.data
-            })
-        } catch (error) {
-            console.log(error)
-            alert('Could not delete user')
-        }
+export function deleteUser(id) {
+  return async function dispatch() {
+    try {
+      const json = await axios.delete(`http://localhost:3001/user/${id}`);
+      return dispatch({
+        type: "DELETE_USER",
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log(error);
+      alert("Could not delete user");
     }
+  };
 }
 
-export function getPets(){
-    return async function(dispatch){
-        try {
-            const json = await axios('http://localhost:3001/pets');
-            return dispatch({
-                type: 'GET_PETS',
-                payload: json.data
-            })
-        } catch (error) {
-            console.log(error);
-            alert('No pets found :/')
-        }
+export function getPets() {
+  return async function (dispatch) {
+    try {
+      const json = await axios("http://localhost:3001/pets");
+      return dispatch({
+        type: "GET_PETS",
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log(error);
+      alert("No pets found :/");
     }
+  };
 }
 
-export function getPetNames(name){
-    return async function(dispatch){
-        try {
-            const json = await axios(`http://localhost:3001/pets?name=${name}`);
-            return dispatch({
-                type: 'GET_NAMES',
-                payload: json.data
-            })
-        } catch (error) {
-            console.log(error);
-            alert('No pet found with that name :/')
-        }
+export function getPetNames(name) {
+  return async function (dispatch) {
+    try {
+      const json = await axios(`http://localhost:3001/pets?name=${name}`);
+      return dispatch({
+        type: "GET_NAMES",
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log(error);
+      alert("No pet found with that name :/");
     }
+  };
 }
 
-export function postPet(payload){
-    return async function(dispatch){
-        await axios.post(`http://localhost:3001/pets`, payload)
-    }
+export function postPet(payload) {
+  return async function (dispatch) {
+    await axios.post(`http://localhost:3001/pets`, payload);
+  };
 }
 
-export function getDetails(id){
-    return async function(dispatch){
-        try {
-            const json = await axios(`http://localhost:3001/pets/${id}`);
-            return dispatch({
-                type: 'GET_DETAILS',
-                payload: json.data
-            })
-        } catch (error) {
-            console.log(error);
-            alert('No pet found');
-        }
+export function getDetails(id) {
+  return async function (dispatch) {
+    try {
+      const json = await axios(`http://localhost:3001/pets/${id}`);
+      return dispatch({
+        type: "GET_DETAILS",
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log(error);
+      alert("No pet found");
     }
+  };
 }
 
-export function deletePet(id){
-    return async function(dispatch){
-        try {
-            const json = await axios.delete(`http://localhost:3001/pets/${id}`);
-            return dispatch({
-                type: 'DELETE_PET',
-                payload: json.data
-            })
-        } catch (error) {
-            console.log(error);
-            alert('Could not delete pet')
-        }
+export function deletePet(id) {
+  return async function (dispatch) {
+    try {
+      const json = await axios.delete(`http://localhost:3001/pets/${id}`);
+      return dispatch({
+        type: "DELETE_PET",
+        payload: json.data,
+      });
+    } catch (error) {
+      console.log(error);
+      alert("Could not delete pet");
     }
+  };
 }
 
-export function resetDetails(){
-    return {
-        type: 'RESET_DETAILS'
-    }
+export function resetDetails() {
+  return {
+    type: "RESET_DETAILS",
+  };
 }
 
-export function orderBySex(payload) {
-    return {
-        type: 'FILTER_BY_SEX',
-        payload
-    }
+export function filterBySex(payload) {
+  return {
+    type: "FILTER_BY_SEX",
+    payload,
+  };
 }
 
-export function orderBySize(payload) {
-    return {
-        type: 'FILTER_BY_SIZE',
-        payload
-    }
+export function filterBySize(payload) {
+  return {
+    type: "FILTER_BY_SIZE",
+    payload,
+  };
 }
 
 export function orderByAge(payload) {
-    return {
-        type: 'ORDER_BY_AGE',
-        payload
-    }
+  return {
+    type: "ORDER_BY_AGE",
+    payload,
+  };
 }
 
-export function orderByLocation(payload) {
-    return {
-        type: 'FILTER_BY_LOCATION',
-        payload
-    }
+export function filterByLocation(payload) {
+  return {
+    type: "FILTER_BY_LOCATION",
+    payload,
+  };
 }
