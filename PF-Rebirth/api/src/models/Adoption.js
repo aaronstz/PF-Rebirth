c6ce12d469c75e386f92b50c585652c41be31dc7
Mariel Gramajo <marielgramajo@MacBook-Air-de-Mariel.local>
2022-07-26 16:36:42 -0300

    feat(routes/successStories) agregue rutas, modifique el modelo success

--- PF-Rebirth/api/src/models/Adoption.js
+++ PF-Rebirth/api/src/models/Adoption.js
@@ -5,7 +5,8 @@ module.exports = (sequelize) => {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
-      allowNull: false
+      allowNull: false,
+      primaryKey: true,
     },
     age: {
       type: DataTypes.INTEGER,
--- PF-Rebirth/api/src/routes/successStories.js
+++ PF-Rebirth/api/src/routes/successStories.js
@@ -1,2 +1,45 @@
 const { Router } = require("express");
+const { SuccessStories } = require("../db");
+const router = Router();
+
+router.post("/", async (req, res, next) => {
+  const { id, nameOfPet, imageOfPet, rating, testimonio, show } = req.body;
+  try {
+    await SuccessStories.create({
+      id,
+      nameOfPet,
+      imageOfPet,
+      rating,
+      testimonio,
+      show,
+    });
+    res.status(200).send(`the testimony was created successfully`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const testimonios = await SuccessStories.findAll();
+    res.send(testimonios);
+  } catch (error) {
+    next(error);
+  }
+});
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const successDelete = await SuccessStories.findByPk(id);
+    if (!successDelete) {
+      res.status(404).send(`testimony not found`);
+    } else {
+      await SuccessStories.destroy({ where: { id: id } });
+      res.status(200).send("the testimony was removed");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
