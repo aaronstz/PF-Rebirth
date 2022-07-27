63a83fded1ce1c33813058098888ca255c1adea4
Aaron <aaronstz@hotmail.com>
2022-07-27 17:10:08 -0300

    probando deploy

--- PF-Rebirth/api/index.js
+++ PF-Rebirth/api/index.js
@@ -24,7 +24,7 @@ const { PORT } = process.env
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
   await getPets()
-  server.listen(PORT, () => {
+  server.listen(PORT || 3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
