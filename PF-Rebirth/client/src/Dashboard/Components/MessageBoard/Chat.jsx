eb559b161deafe204206eae0ea12f3fda73dd30c
Carlos Candioti <carlos.candioti@gmail.com>
2022-07-29 11:48:51 -0300

    checkeando

--- PF-Rebirth/client/src/Dashboard/Components/MessageBoard/Chat.jsx
+++ PF-Rebirth/client/src/Dashboard/Components/MessageBoard/Chat.jsx
@@ -5,20 +5,20 @@ export default function Chat({id,userId,userPhoto,userId2,userPhoto2,conversatio
 
     return (
         <React.Fragment>
-            <div className="chat-container">
+            <div className="chat-container chat-font-size">
                 <div className="chat-display">
                     {conversation.map(chat=>{
                     let newMsg=chat.new? "New":""
                     newMsg && advisor("New")
                     return chat.userMsgId===userId?<div  className="chat-right">{chat.msg}</div>
-                    : <div className="chat-display"> <div className="chat-new">{newMsg}</div> <div  className="chat-left">{chat.msg}</div></div>                    
+                    : <div className="chat-left-container"> <div className="chat-new">{newMsg}</div> <div  className="chat-left">{chat.msg}</div></div>                    
                     })}
                    
                 </div>
                 
             </div>
-            <div className="chat-input-container">
-                    <input className="chat-input" type="text" />
+            <div className="chat-input-container chat-font-size">
+                    <textarea className="chat-input"  />
                     <button className="chat-button"></button>
                 </div>
         </React.Fragment>
