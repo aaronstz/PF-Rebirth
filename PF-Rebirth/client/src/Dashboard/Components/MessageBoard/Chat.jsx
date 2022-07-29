c19b41161b3db35c4fd78cb939a0b3ecf6481bb7
Carlos Candioti <carlos.candioti@gmail.com>
2022-07-29 19:38:27 -0300

    Chat visual completo

--- PF-Rebirth/client/src/Dashboard/Components/MessageBoard/Chat.jsx
+++ PF-Rebirth/client/src/Dashboard/Components/MessageBoard/Chat.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Chat.css'
+import sendButton from '../../../Assets/Messageboard/send.svg'
 
 export default function Chat({id,userId,userPhoto,userId2,userPhoto2,conversation,advisor}){
 
@@ -8,7 +9,7 @@ export default function Chat({id,userId,userPhoto,userId2,userPhoto2,conversatio
             <div className="chat-container chat-font-size">
                 <div className="chat-display">
                     {conversation.map(chat=>{
-                    let newMsg=chat.new? "New":""
+                    let newMsg=chat.new? "--New--":""
                     newMsg && advisor("New")
                     return chat.userMsgId===userId?<div  className="chat-right">{chat.msg}</div>
                     : <div className="chat-left-container"> <div className="chat-new">{newMsg}</div> <div  className="chat-left">{chat.msg}</div></div>                    
@@ -19,7 +20,7 @@ export default function Chat({id,userId,userPhoto,userId2,userPhoto2,conversatio
             </div>
             <div className="chat-input-container chat-font-size">
                     <textarea className="chat-input"  />
-                    <button className="chat-button"></button>
+                    <button className="chat-button"><img src={sendButton}></img> </button>
                 </div>
         </React.Fragment>
     )
