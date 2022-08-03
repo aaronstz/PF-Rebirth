989758ddca7019cce60f1539443b3cec17dc5992
LennySnaider <lenny_snaiderman@yahoo.com>
2022-08-03 10:45:14 -0500

    fix landing

--- PF-Rebirth/client/src/Pages/LandingPage.jsx
+++ PF-Rebirth/client/src/Pages/LandingPage.jsx
@@ -3,33 +3,34 @@ import logo from "../Assets/logoLanding.png";
 import cat from "../Assets/catTeam.png";
 import dog from "../Assets/dogTeam.png";
 import "./LandingPage.css";
-import Container from "react-bootstrap/esm/Container";
+import "../index.css";
+//import Container from "react-bootstrap/esm/Container";
 
 export default function LandingPage() {
   return (
-    <body>
-      <Container>
-        <br />
-        <br />
-        <div className="logo-ld">
-          <img src={logo} alt="Rebirth" />
-        </div>
-        <div className="title-ld">
-          <h2>A NEW LIFE OPPORTUNITY </h2>
-        </div>
-        <div className="pet-ld">
+    <div className="containerLand">
+      <div className="logo-ld">
+        <img src={logo} alt="Rebirth" />
+      </div>
+      <div className="title-ld">
+        <h2>A NEW LIFE OPPORTUNITY </h2>
+      </div>
+      <div className="pet-ld">
+        <div className="wrapperLeftLand">
           <Link to="/home?type=dog">
             <img src={dog} alt="Dog Team" />
           </Link>
+        </div>
+        <div className="wrapperRightLand">
           <Link to="/home?type=cat">
             <img src={cat} alt="Cat Team" />
           </Link>
         </div>
-        <div className="sub-ld">
-          <br />
-          <h3>PLEASE CHOOSE YOUR TEAM</h3>
-        </div>
-      </Container>
-    </body>
+      </div>
+      <div className="sub-ld">
+        <h3>PLEASE CHOOSE YOUR TEAM</h3>
+      </div>
+      <div className="Loginfooter">Copyright &copy; 2022 &mdash; Team 13</div>
+    </div>
   );
 }
