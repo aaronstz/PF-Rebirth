37768aed64693977ae1d2758aaa344accd0c8475
Carlos Candioti <carlos.candioti@gmail.com>
2022-08-05 18:00:19 -0300

    Chat muestra adoptante y mascota

--- PF-Rebirth/client/src/Dashboard/Components/MyAdoptions/Components/Accept-Reject/Accept-Reject.jsx
+++ PF-Rebirth/client/src/Dashboard/Components/MyAdoptions/Components/Accept-Reject/Accept-Reject.jsx
@@ -1,41 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Accept-Reject.css";
 import image from "../../../../../Assets/fotoPet1.png";
-
+import { useSelector } from "react-redux";
 const AcceptReject = () => {
+
+const adoptChat=useSelector((state)=>state.adoptionChat)
+const adoptionId=useSelector((state)=>state.adoptionId)
+const infoStorage = localStorage.getItem("user");
+    const user = JSON.parse(infoStorage)
+    const mail= user.mail
+  
+
+
+console.log()
+
+
   return (
-    <div className="mainDashCont">
+    <>
+    {  adoptChat.filter((adChat)=>adChat.id===adoptionId).map((datos)=>{
+    if (datos.owner.mail===mail){
+    return <div className="mainDashCont">
         <div className="AdoptContainer">
         <div >
           <div class="imgFav">
-            <img src={image} alt="Pet" class="img" />
+            <img  src={datos.adopter.image} alt="Adopter" class="img" />
           </div>
         </div>
         <div >
-          <span>Lolita</span>
+          <span>Name: {datos.adopter.name} {datos.adopter.lastName}</span>
           <br />
-          <span>Border collie</span>
+          <span>Email: {datos.adopter.mail}</span>
           <br />
-          <span>3&nbsp;years</span>
+          <span>Gender: {datos.gender}</span>
           <br />
-          <span>Mexico City</span>
+          <span>Address: {datos.address}</span>
+          <br />
+          <span>Other pets: {datos.otherpets}</span>
+          <br />
+          <span>Phone: {datos.phone}</span>
         </div>
-        <div>
-          
-          <span>Female</span>
-          <br />
-          <span>Medium</span>
-          <br />
-          <span>3.4&nbsp;KG</span>
         </div>
-        <div >
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-            turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
-            nec fringilla accumsan, risus sem
-          </span>
-        </div>
-      </div>
       <div className="btnRowAdopt">
         <button class="MAdoCancbutton">
           <span>Reject</span>
@@ -45,6 +49,47 @@ const AcceptReject = () => {
         </button>
       </div>
     </div>
+    }else{
+     return <div className="mainDashCont">
+     <div className="AdoptContainer">
+     <div >
+       <div class="imgFav">
+         <img src={datos.pet.image} alt="Pet" class="img" />
+       </div>
+     </div>
+     <div >
+       <span>Name: {datos.pet.name}</span>
+       <br />
+       <span>Race: {datos.pet.race}</span>
+       <br />
+       <span>Age: {datos.pet.age} years</span>
+       <br />
+       <span>Location: {datos.pet.location}</span>
+       <br />
+       <span>Gender: {datos.pet.gender}</span>
+       <br />
+       <span>Size: {datos.pet.size}</span>
+       <br />
+       <span>Description: {datos.pet.description}</span>
+     </div>
+       </div>
+   <div className="btnRowAdopt">
+     <button class="MAdoCancbutton">
+       <span>Reject</span>
+     </button>
+     <button class="MAdoptbutton">
+       <span>Accept</span>
+     </button>
+   </div>
+ </div>
+
+    }
+
+    }
+    )
+    
+    }
+    </>
   );
 };
 
