145db35b4a52ed26e52d257a579bba17d1d588a0
Mariel Gramajo <marielgramajo@MacBook-Air-de-Mariel.local>
2022-08-05 18:03:55 -0300

    aun no

--- PF-Rebirth/client/src/Dashboard/Components/MyAdoptions/Components/Accept-Reject/Accept-Reject.jsx
+++ PF-Rebirth/client/src/Dashboard/Components/MyAdoptions/Components/Accept-Reject/Accept-Reject.jsx
@@ -1,50 +1,67 @@
 import React from "react";
 import "./Accept-Reject.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import image from "../../../../../Assets/fotoPet1.png";
+import { getChat } from "../../../../../Redux/Actions";
 
-const AcceptReject = () => {
+const AcceptReject = (adoptionId) => {
+  const adoptChat = useSelector((state) => state.adoptionChat);
+  const dispatch = useDispatch();
+  const infoStorage = localStorage.getItem("user");
+  const user = JSON.parse(infoStorage);
+  const mail = user.mail;
+  console.log(adoptChat);
+
+  useEffect(() => {
+    dispatch(getChat(mail));
+  }, [dispatch]);
+
   return (
-    <div className="mainDashCont">
-        <div className="AdoptContainer">
-        <div >
-          <div class="imgFav">
-            <img src={image} alt="Pet" class="img" />
+    <>
+      {adoptChat.map((adChat) => {
+        <div className="mainDashCont">
+          <div className="AdoptContainer">
+            <div>
+              <div class="imgFav">
+                <img src={adChat.pet.image} alt="Pet" class="img" />
+              </div>
+            </div>
+            <div>
+              <span>Lolita</span>
+              <br />
+              <span>Border collie</span>
+              <br />
+              <span>3&nbsp;years</span>
+              <br />
+              <span>Mexico City</span>
+            </div>
+            <div>
+              <span>Female</span>
+              <br />
+              <span>Medium</span>
+              <br />
+              <span>3.4&nbsp;KG</span>
+            </div>
+            <div>
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                eu turpis molestie, dictum est a, mattis tellus. Sed dignissim,
+                metus nec fringilla accumsan, risus sem
+              </span>
+            </div>
           </div>
-        </div>
-        <div >
-          <span>Lolita</span>
-          <br />
-          <span>Border collie</span>
-          <br />
-          <span>3&nbsp;years</span>
-          <br />
-          <span>Mexico City</span>
-        </div>
-        <div>
-          
-          <span>Female</span>
-          <br />
-          <span>Medium</span>
-          <br />
-          <span>3.4&nbsp;KG</span>
-        </div>
-        <div >
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-            turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
-            nec fringilla accumsan, risus sem
-          </span>
-        </div>
-      </div>
-      <div className="btnRowAdopt">
-        <button class="MAdoCancbutton">
-          <span>Reject</span>
-        </button>
-        <button class="MAdoptbutton">
-          <span>Accept</span>
-        </button>
-      </div>
-    </div>
+          <div className="btnRowAdopt">
+            <button class="MAdoCancbutton">
+              <span>Reject</span>
+            </button>
+            <button class="MAdoptbutton">
+              <span>Accept</span>
+            </button>
+          </div>
+        </div>;
+      })}
+    </>
   );
 };
 
--- PF-Rebirth/client/src/Dashboard/Components/MyAdoptions/Components/Adoption-request/LateralBar.jsx
+++ PF-Rebirth/client/src/Dashboard/Components/MyAdoptions/Components/Adoption-request/LateralBar.jsx
@@ -1,49 +1,72 @@
 import React from "react";
 import { useEffect } from "react";
-import {useDispatch, useSelector} from "react-redux"
-import avatar from '../../../../../Assets/Messageboard/avatar.png'
-import mascota from '../../../../../Assets/Messageboard/pet.png'
+import { useDispatch, useSelector } from "react-redux";
+import avatar from "../../../../../Assets/Messageboard/avatar.png";
+import mascota from "../../../../../Assets/Messageboard/pet.png";
 import { getChat, saveAdoptionId } from "../../../../../Redux/Actions";
-import './LateralBar.css'
+import "./LateralBar.css";
 import { getMessage } from "../../../../../Redux/Actions";
 
+export default function LateralBar() {
+  const adoptChat = useSelector((state) => state.adoptionChat);
+  const dispatch = useDispatch();
+  const infoStorage = localStorage.getItem("user");
+  const user = JSON.parse(infoStorage);
+  const mail = user.mail;
+  console.log(user.mail, mail);
+  console.log(adoptChat);
+  useEffect(() => {
+    dispatch(getChat(mail));
+  }, [dispatch]);
 
+  function handleClick(adoptionId) {
+    dispatch(getMessage(adoptionId));
+    dispatch(saveAdoptionId(adoptionId));
+  }
 
-export default function LateralBar(){
-const adoptChat= useSelector((state)=> state.adoptionChat)
-const dispatch= useDispatch();
-const infoStorage = localStorage.getItem("user");
-    const user = JSON.parse(infoStorage)
-    const mail=  user.mail
-    console.log(user.mail,mail)   
-useEffect(()=>{
-  dispatch(getChat(mail))
-},[dispatch])
-    
-function handleClick(adoptionId){
-dispatch(getMessage(adoptionId))
-dispatch(saveAdoptionId(adoptionId))
-
-}
-
-    return(<>
-
-        {adoptChat.map((adChat)=>{
-        
-        return <div onClick={()=>handleClick(adChat.id)} className="container-lateral-bar">
+  return (
+    <>
+      {adoptChat.map((adChat) => {
+        return (
+          <div
+            onClick={() => handleClick(adChat.id)}
+            className="container-lateral-bar"
+          >
             <div>
-            <div className="avatar-pet-lateral-bar">
-                <img className="avatar-lateral-bar" src={adChat.adopter.mail===mail? adChat.adopter.image:adChat.owner.image} alt="" /> 
-                <img className="pet-lateral-bar" src={adChat.pet.image} alt="" />
-            </div>
+              <div className="avatar-pet-lateral-bar">
+                <img
+                  className="avatar-lateral-bar"
+                  src={
+                    adChat.adopter.mail === mail
+                      ? adChat.adopter.image
+                      : adChat.owner.image
+                  }
+                  alt=""
+                />
+                <img
+                  className="pet-lateral-bar"
+                  src={adChat.pet.image}
+                  alt=""
+                />
+              </div>
             </div>
             <div className="name-adoption-text-lb">
-                {console.log(adChat)}
-                <div className="name-lateral-bar"> {adChat.userMail===mail? adChat.owner.name:adChat.adopter.name}</div>
-                <div className="adoption-lateral-bar"> Adoption<br /></div>
+              {console.log(adChat)}
+              <div className="name-lateral-bar">
+                {" "}
+                {adChat.userMail === mail
+                  ? adChat.owner.name
+                  : adChat.adopter.name}
+              </div>
+              <div className="adoption-lateral-bar">
+                {" "}
+                Adoption
+                <br />
+              </div>
             </div>
-         </div>
-         })
-         }
-        </>)
+          </div>
+        );
+      })}
+    </>
+  );
 }
