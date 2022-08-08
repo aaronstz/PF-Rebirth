73274ae4bc3b391d514424c21a1c2309291ad1b0
Mariel Gramajo <marielgramajo@MacBook-Air-de-Mariel.local>
2022-08-08 12:35:15 -0300

    feat-maru-footer, modifique el about y redireccione

--- PF-Rebirth/client/src/Components/Footer/Footer.jsx
+++ PF-Rebirth/client/src/Components/Footer/Footer.jsx
@@ -4,103 +4,98 @@ import socials from "../../Assets/Social.png";
 import { NavLink } from "react-router-dom";
 
 export default function Footer() {
-
   const user = localStorage.getItem("user");
 
   return (
     <footer className="body">
       <div className="upperContainer">
         <div className="containerFooter mt-5">
-          
-            <div>
-              <NavLink to={"/home"} className={"footer-link"}>
-                  <img src={logo} alt="Rebirth"></img>
+          <div>
+            <NavLink to={"/home"} className={"footer-link"}>
+              <img src={logo} alt="Rebirth"></img>
+            </NavLink>
+            <div className="sub-title">
+              <p>
+                Pet adoption network, to give
+                <br /> them a new life opportunity.
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <ul className="list-unstyled">
+              Company
+              <li className="nav-item">
+                <NavLink to={"/about"}>About us</NavLink>
+              </li>
+              <li className="nav-item">
+                <a href=" ">Testimonials</a>
+              </li>
+              <li className="nav-item">
+                <NavLink to={"/support"}>Contact Us</NavLink>
+              </li>
+              <li className="nav-item">
+                <a href={user !== null ? "/donations" : "/login"}>Donate</a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <ul className="list-unstyled">
+              Services
+              <li className="nav-item">
+                <NavLink to={"/faqs"}>F. A. Q. s</NavLink>
+              </li>
+              <li className="nav-item">
+                <a href={user !== null ? "/favorites" : "/login"}>
+                  My Favorites
+                </a>
+              </li>
+              <li className="nav-item">
+                <NavLink to={user !== null ? "/profile" : "/login"}>
+                  My account
                 </NavLink>
-                <div className="sub-title">
-                  <p>
-                    Pet adoption network, to give
-                    <br /> them a new life opportunity.
-                  </p>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <ul className="list-unstyled">
+              Get in Touch
+              <li className="nav-item">
+                <div className="paragraph">
+                  Subscribe to our weekly <br /> newsletter and receive
+                  <br />
+                  updates via email.
                 </div>
-            </div>
-
-
-            <div>
-                <ul className="list-unstyled">
-                  Company
-                  <li className="nav-item">
-                    <a href=" ">About us</a>
-                  </li>
-                  <li className="nav-item">
-                    <a href=" ">Testimonials</a>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink to={"/support"}>Contact Us</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <a href={user!==null ? "/donations" : "/login"}>Donate</a>
-                  </li>
-                </ul>
-            </div>
-
-
-            <div>
-              <ul className="list-unstyled">
-                    Services
-                    <li className="nav-item">
-                      <NavLink to={"/faqs"}>F. A. Q. s</NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <a href={user!==null ? "/favorites" : "/login"}>My Favorites</a>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink to={user!==null ? "/profile" : "/login"}>My account</NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <a href=" ">Terms & Conditions</a>
-                    </li>
-              </ul>
-            </div>
-
-
-            <div>
-              <ul className="list-unstyled">
-                Get in Touch
-                <li className="nav-item">
-                  <div className="paragraph">
-                    Subscribe to our weekly <br /> newsletter and receive
-                    <br />
-                    updates via email.
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <a href=" " className="nav-link pl-0">
-                    <img src={socials} alt="Socials" className="socials" />
-                  </a>
-                </li>
-              </ul>
-            </div>
+              </li>
+              <li className="nav-item">
+                <a href=" " className="nav-link pl-0">
+                  <img src={socials} alt="Socials" className="socials" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
 
       <div className="downContainer">
         <div className="lower">
-            <h3 className="rights">All Rights Reserved @ Team 13</h3>
-            <ul className="footer-bottom">
-              <li className="li-bottom">
-                <span className="a-bottom">
-                  <NavLink to={"/terms"}>Terms & Conditions</NavLink>
-                </span>
-              </li>
-              <li className="li-bottom">
-                <span className="a-bottom">
-                  <NavLink to={"/privacy"}>Privacy & Policy</NavLink>
-                </span>
-              </li>
-            </ul>
+          <h3 className="rights">All Rights Reserved @ Team 13</h3>
+          <ul className="footer-bottom">
+            <li className="li-bottom">
+              <span className="a-bottom">
+                <NavLink to={"/terms"}>Terms & Conditions</NavLink>
+              </span>
+            </li>
+            <li className="li-bottom">
+              <span className="a-bottom">
+                <NavLink to={"/privacy"}>Privacy & Policy</NavLink>
+              </span>
+            </li>
+          </ul>
         </div>
       </div>
-
     </footer>
   );
 }
