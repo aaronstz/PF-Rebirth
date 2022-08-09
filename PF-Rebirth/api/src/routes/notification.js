2d5f8fb985cb5de6c279c3579556a21228ac465e
tobiastymkiw <tobiastymkiw@gmail.com>
2022-08-09 19:18:16 -0300

    rutas funcionando

--- PF-Rebirth/api/src/routes/notification.js
+++ PF-Rebirth/api/src/routes/notification.js
@@ -1,50 +1,55 @@
 const router = require("express").Router();
-const {Notification}=require("../db")
+const { Notification } = require("../db");
 
-
-router.get("/:mail", async (res,req,next)=>{
-try {
-    const {mail}= req.params;
-    let noti= await Notification.findOne({where:{mail:mail}})
-    res.status(200).send(noti.nuevo)
-
-} catch (error) {
-    next(error)
-}
-
-
-})
-
-
-
-router.post("/", async (res,req,next)=>{
-try {
-    const {mail} = req.body;
-    let noti= await Notification.findOrCreate({where:{mail:userMail}})
-    noti.nuevo=true;
-   await noti.save();
-res.status(200).send("Notificacion actualizada")
-
-} catch (error) {
-    next(error)
-}
-}
-)
-
-router.patch("/", async (res,req,next)=>{
-    try {
-        const {userMail}= req.body;
-        let noti= await Notification.findOne({where:{mail:userMail}})
-        noti.nuevo=false;
-       await noti.save();
-    res.status(200).send("Notificacion actualizada")
-    
-    } catch (error) {
-        next(error)
+router.get("/:mail", async (req, res, next) => {
+  try {
+    const { mail } = req.params;
+    if (mail.length > 3) {
+      const notification = await Notification.findOne({
+        where: { userMail: mail },
+      });
+      res.status(200).send(notification);
+    } else {
+      const notification = await Notification.findAll();
+      res.status(200).send(notification);
     }
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
+router.post("/", async (req, res, next) => {
+  try {
+    const { userMail } = req.body;
+    const notification = await Notification.findOne({
+      where: { userMail },
+    });
+    if (notification) {
+      notification.nuevo = true;
+      await notification.save();
+      res.status(201).send("Notification updated");
+    } else {
+      await Notification.findOrCreate({
+        where: { userMail },
+        default: { nuevo: true },
+      });
+      res.status(200).send("Notification created");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-
+router.patch("/", async (req, res, next) => {
+  try {
+    const { userMail } = req.body;
+    const noti = await Notification.findOne({ where: { userMail: userMail } });
+    noti.nuevo = false;
+    await noti.save();
+    res.status(200).send("Notification updated");
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
