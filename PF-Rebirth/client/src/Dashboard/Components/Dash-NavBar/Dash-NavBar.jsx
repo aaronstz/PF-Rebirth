250536fd7da366dbd3e83e84fb9519550e88cce2
LennySnaider <lenny_snaiderman@yahoo.com>
2022-08-09 15:40:21 -0500

    fix foto perfil navbar, fix, perfil no scrolea, feat componente not found

--- PF-Rebirth/client/src/Dashboard/Components/Dash-NavBar/Dash-NavBar.jsx
+++ PF-Rebirth/client/src/Dashboard/Components/Dash-NavBar/Dash-NavBar.jsx
@@ -9,9 +9,7 @@ import DarkMode from "../../../Components/Switch/SwitchMode";
 import { NavLink } from "react-router-dom";
 import "./Dash-NavBar.css";
 
-
 const DashNavBar = () => {
-
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -19,7 +17,7 @@ const DashNavBar = () => {
   }, []);
 
   let data = user ? JSON.parse(user) : null;
-  console.log(data)
+  console.log(data);
   return (
     <>
       <div className="DashcontainerNavbar">
@@ -28,50 +26,67 @@ const DashNavBar = () => {
         </NavLink>
 
         <div className="DashiconsContainer">
-        <div className="Dashitem">
-            <NavLink to={data !== null ? "/historial" : "/login"} className="Dashlink-navbar">
+          <div className="Dashitem">
+            <NavLink
+              to={data !== null ? "/historial" : "/login"}
+              className="Dashlink-navbar"
+            >
               <img src={vector5} alt="vector3" className="Dashicons" />
               <span>Historial</span>
             </NavLink>
           </div>
           <div className="Dashitem">
-            <NavLink to={data !== null ? "/request" : "/login"} className="Dashlink-navbar">
+            <NavLink
+              to={data !== null ? "/request" : "/login"}
+              className="Dashlink-navbar"
+            >
               <img src={vector4} alt="vector3" className="Dashicons" />
               <span>MessageBoard</span>
             </NavLink>
           </div>
           <div className="Dashitem">
-            <NavLink to={data !== null ? "/create" : "/login"} className="Dashlink-navbar">
-
+            <NavLink
+              to={data !== null ? "/create" : "/login"}
+              className="Dashlink-navbar"
+            >
               <img src={vector3} alt="vector3" className="Dashicons" />
               <span>USERS</span>
-            </NavLink> 
-        <div className="DashiconsContainer">
-          <div className="Dashitem">
-              <NavLink to={"/login"} className="Dashlink-navbar">
-                <img src={vector3} alt="vector3" className="Dashicons" />
-                <span>New Pet</span>
+            </NavLink>
+            <div className="DashiconsContainer">
+              <div className="Dashitem">
+                <NavLink to={"/login"} className="Dashlink-navbar">
+                  <img src={vector3} alt="vector3" className="Dashicons" />
+                  <span>New Pet</span>
+                </NavLink>
+              </div>
+              <div className="Dashitem">
+                <NavLink
+                  to={data !== null ? "/favorites" : "/login"}
+                  className="Dashlink-navbar"
+                >
+                  <img src={vector2} alt="vector2" className="Dashicons" />
+                  <span>My favorites</span>
+                </NavLink>
+              </div>
+              <div className="Dashitem">
+                <DarkMode />
+              </div>
+              <NavLink
+                to={data !== null ? "/profile" : "/login"}
+                className="Dashlink-navbar"
+              >
+                <span className="DashuserName">{data && data.name}</span>
+                <img
+                  className="DashProfile"
+                  src={data && data.image}
+                  alt="avatar"
+                />
               </NavLink>
+            </div>
           </div>
-          <div className="Dashitem">
-            <NavLink to={data !== null ? "/favorites" : "/login"} className="Dashlink-navbar">
-              <img src={vector2} alt="vector2" className="Dashicons" />
-              <span>My favorites</span>
-            </NavLink> 
-          </div>
-          <div className="Dashitem">
-            <DarkMode />
-          </div>
-          <NavLink to={data !== null ? "/profile" : "/login"} className="Dashlink-navbar">
-            <span className="DashuserName">{data && data.name}</span>
-            <img src={data&& data.image} alt="avatar" />
-          </NavLink>
         </div>
-        </div>
-      </div>
       </div>
     </>
-
   );
 };
 
