ba1ed9a6fa0c5d4ef3d4e284a8920ebf8ceac18b
LennySnaider <lenny_snaiderman@yahoo.com>
2022-08-10 08:16:49 -0500

    fix create form

--- PF-Rebirth/client/src/Dashboard/Components/AddNew/AddNew.jsx
+++ PF-Rebirth/client/src/Dashboard/Components/AddNew/AddNew.jsx
@@ -108,222 +108,228 @@ function AddNew() {
 
   return (
     <section>
-    <DashNavBar/>
-      <div className="add-container">
-        <div className="add-wrapper">
-          <div className="add-wrapperleft">
-            <div className="add-md-center">
-              <div className="addcard-wrapper">
-                <div className="addcard fat">
-                  <div className="addcard header">
-                    <div className="mb-3" id="formBasicTitle">
-                      <h2>Add new pet</h2>
-                      Please leave us the following info to create a new pet
-                    </div>
-                    <div className="addcard-body">
-                      <form
-                        method="POST"
-                        className="addvalidate"
-                        noValidate=""
-                        onSubmit={(e) => handleSubmit(e)}
-                      >
-                        <div className="">
-                          <label htmlFor="name">Name*</label>
-                          <input
-                            id="name"
-                            type="text"
-                            className="form-control"
-                            name="name"
-                            value={input.name}
-                            required
-                            autoFocus
-                            onChange={(e) => {
-                              handleChange(e);
-                            }}
-                          />
-                          <div className="addinvalid-fb">
-                            {errors && errors.name ? errors.name : null}
-                          </div>
-                        </div>
-
-                        <div className="addform">
-                          <label className="form-label" htmlFor="customFile">
-                            Image*
-                          </label>
-                          <Widget
-                            publicKey="e7afc989eff083e04496"
-                            value={input.image}
-                            required
-                            onFileSelect={(e) => {
-                              e.done((file) => {
-                                handleImage(file);
-                              });
-                            }}
-                          />
-                          <div className="addinvalid-fb">
-                            {errors && errors.image ? errors.image : null}
-                          </div>
-                        </div>
-                        <div className="formControlRow">
-                          <div className="addformSm">
-                            <label htmlFor="age">Age*</label>
+      <DashNavBar />
+      <div className="DashcontainerMain">
+        <div className="add-container">
+          <div className="add-wrapper">
+            <div className="add-wrapperleft">
+              <div className="add-md-center">
+                <div className="addcard-wrapper">
+                  <div className="addcard fat">
+                    <div className="addcard header">
+                      <div className="mb-3" id="formBasicTitle">
+                        <h2>Add new pet</h2>
+                        Please leave us the following info to create a new pet
+                      </div>
+                      <div className="addcard-body">
+                        <form
+                          method="POST"
+                          className="addvalidate"
+                          noValidate=""
+                          onSubmit={(e) => handleSubmit(e)}
+                        >
+                          <div className="">
+                            <label htmlFor="name">Name*</label>
                             <input
-                              id="age"
-                              type="number"
-                              className="form-controlSm"
-                              name="age"
-                              value={input.age}
+                              id="name"
+                              type="text"
+                              className="form-control"
+                              name="name"
+                              value={input.name}
                               required
-                              min="0"
+                              autoFocus
                               onChange={(e) => {
                                 handleChange(e);
                               }}
                             />
                             <div className="addinvalid-fb">
-                              {errors && errors.age ? errors.age : null}
+                              {errors && errors.name ? errors.name : null}
                             </div>
                           </div>
 
-                          <div className="addformSm">
-                            <label htmlFor="exampleFormControlSelect1">
-                              Size*
+                          <div className="addform">
+                            <label className="form-label" htmlFor="customFile">
+                              Image*
                             </label>
-                            <select
-                              name="size"
-                              className="form-controlSm"
-                              id="exampleFormControlSelect1"
-                              value={input.size}
-                              onChange={(e) => handleChange(e)}
-                            >
-                              <option defaultValue>Choose...</option>
-                              <option value="small">Small</option>
-                              <option value="medium">Medium</option>
-                              <option value="big">Big</option>
-                            </select>
+                            <Widget
+                              publicKey="e7afc989eff083e04496"
+                              value={input.image}
+                              required
+                              onFileSelect={(e) => {
+                                e.done((file) => {
+                                  handleImage(file);
+                                });
+                              }}
+                            />
+                            <div className="addinvalid-fb">
+                              {errors && errors.image ? errors.image : null}
+                            </div>
                           </div>
+                          <div className="formControlRow">
+                            <div className="addformSm">
+                              <label htmlFor="age">Age*</label>
+                              <input
+                                id="age"
+                                type="number"
+                                className="form-controlSm"
+                                name="age"
+                                value={input.age}
+                                required
+                                min="0"
+                                onChange={(e) => {
+                                  handleChange(e);
+                                }}
+                              />
+                              <div className="addinvalid-fb">
+                                {errors && errors.age ? errors.age : null}
+                              </div>
+                            </div>
 
-                          <div className="addformSm">
-                            <label htmlFor="exampleFormControlSelect1">
-                              Gender*
-                            </label>
+                            <div className="addformSm">
+                              <label htmlFor="exampleFormControlSelect1">
+                                Size*
+                              </label>
+                              <select
+                                name="size"
+                                className="form-controlSm"
+                                id="exampleFormControlSelect1"
+                                value={input.size}
+                                onChange={(e) => handleChange(e)}
+                              >
+                                <option defaultValue>Choose...</option>
+                                <option value="small">Small</option>
+                                <option value="medium">Medium</option>
+                                <option value="big">Big</option>
+                              </select>
+                            </div>
+
+                            <div className="addformSm">
+                              <label htmlFor="exampleFormControlSelect1">
+                                Gender*
+                              </label>
+                              <select
+                                name="gender"
+                                className="form-controlSm"
+                                id="exampleFormControlSelect1"
+                                onChange={(e) => {
+                                  handleChange(e);
+                                }}
+                              >
+                                <option defaultValue>Choose...</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                              </select>
+                            </div>
+                            <div className="addformSm">
+                              <label htmlFor="exampleFormControlSelect1">
+                                Type*
+                              </label>
+                              <select
+                                name="type"
+                                className="form-controlSm"
+                                id="exampleFormControlSelect1"
+                                onChange={(e) => {
+                                  handleChange(e);
+                                }}
+                              >
+                                <option defaultValue>Choose...</option>
+                                <option value="dog">Dog</option>
+                                <option value="cat">Cat</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div>
+                            <div className="addinvalid-fb">
+                              {errors && errors.type ? errors.type : null}
+                            </div>
+                          </div>
+                          <div className="form-groupAdd">
+                            <label htmlFor="race">Breed</label>
+                            <input
+                              id="race"
+                              type="text"
+                              className="form-control"
+                              name="race"
+                              value={input.race}
+                              required
+                              onChange={(e) => {
+                                handleChange(e);
+                              }}
+                            />
+                            <div className="addinvalid-fb">
+                              {errors && errors.race ? errors.race : null}
+                            </div>
+                          </div>
+                          <div className="form-groupAdd">
+                            <label htmlFor="location">Location*</label>
                             <select
-                              name="gender"
-                              className="form-controlSm"
-                              id="exampleFormControlSelect1"
+                              name="location"
+                              className="form-controlAdd"
+                              value={input.location}
                               onChange={(e) => {
                                 handleChange(e);
                               }}
                             >
                               <option defaultValue>Choose...</option>
-                              <option value="male">Male</option>
-                              <option value="female">Female</option>
-                            </select>
-                          </div>
-                          <div className="addformSm">
-                            <label htmlFor="exampleFormControlSelect1">
-                              Type*
-                            </label>
-                            <select
-                              name="type"
-                              className="form-controlSm"
-                              id="exampleFormControlSelect1"
-                              onChange={(e) => {
-                                handleChange(e);
-                              }}
-                            >
-                              <option defaultValue>Choose...</option>
-                              <option value="dog">Dog</option>
-                              <option value="cat">Cat</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div>
-                          <div className="addinvalid-fb">
-                            {errors && errors.type ? errors.type : null}
-                          </div>
-                        </div>
-                        <div className="form-groupAdd">
-                          <label htmlFor="race">Breed</label>
-                          <input
-                            id="race"
-                            type="text"
-                            className="form-control"
-                            name="race"
-                            value={input.race}
-                            required
-                            onChange={(e) => {
-                              handleChange(e);
-                            }}
-                          />
-                          <div className="addinvalid-fb">
-                            {errors && errors.race ? errors.race : null}
-                          </div>
-                        </div>
-                        <div className="form-groupAdd">
-                          <label htmlFor="location">Location*</label>
-                          <select
-                            name="location"
-                            className="form-controlAdd"
-                            value={input.location}
-                            onChange={(e) => {
-                              handleChange(e);
-                            }}
-                          >
-                            <option defaultValue>Choose...</option>
-                            {provincias?.map((p) =>(  
-                              // return (
+                              {provincias?.map((p) => (
+                                // return (
                                 <option key={Math.random()} value={p}>
                                   {p}
                                 </option>
-                              // );
-                            ))}
-                          </select>
-                          <div className="addinvalid-fb">
-                            {errors && errors.location ? errors.location : null}
+                                // );
+                              ))}
+                            </select>
+                            <div className="addinvalid-fb">
+                              {errors && errors.location
+                                ? errors.location
+                                : null}
+                            </div>
                           </div>
-                        </div>
-                        <div className="form-groupAdd">
-                          <label
-                            className="form-label"
-                            htmlFor="textAreaExample"
-                          >
-                            Description
-                          </label>
-                          <textarea
-                            name="description"
-                            value={input.description}
-                            type="text"
-                            className="textarea"
-                            id="textAreaExample"
-                            rows="4"
-                            onChange={(e) => handleChange(e)}
-                          ></textarea>
-                        </div>
-                        <br />
-                        <div className="addform-submit">
-                          <button
-                            type="submit"
-                            className="btn btn-primary addbtn"
-                            disabled={
-                              Object.keys(errors).length === 0 ? false : true
-                            }
-                          >
-                            CREATE
-                          </button>
-                        </div>
-                      </form>
+                          <div className="form-groupAdd">
+                            <label
+                              className="form-label"
+                              htmlFor="textAreaExample"
+                            >
+                              Description
+                            </label>
+                            <textarea
+                              name="description"
+                              value={input.description}
+                              type="text"
+                              className="textarea"
+                              id="textAreaExample"
+                              rows="4"
+                              onChange={(e) => handleChange(e)}
+                            ></textarea>
+                          </div>
+                          <br />
+                          <div className="addform-submit">
+                            <button
+                              type="submit"
+                              className="btn btn-primary addbtn"
+                              disabled={
+                                Object.keys(errors).length === 0 ? false : true
+                              }
+                            >
+                              CREATE
+                            </button>
+                          </div>
+                        </form>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="addwrapper-right">
-            <div className="addimageContainer">
-              <img src={background} alt="pet" className="addImage" />
+            <div className="addwrapper-right">
+              <div className="addimageContainer">
+                <img src={background} alt="pet" className="addImage" />
+              </div>
+            </div>
+            <div className="addfooter">
+              Copyright &copy; 2022 &mdash; Team 13
             </div>
           </div>
-          <div className="addfooter">Copyright &copy; 2022 &mdash; Team 13</div>
         </div>
       </div>
     </section>
@@ -331,7 +337,6 @@ function AddNew() {
 }
 
 export default AddNew;
-
 
 // <div className="addform">
 //                           <label htmlFor="location">Location*</label>
